8193616d85f4f446224080dd81761c2b807ba89a
TheSamsAlif <samsalif852@gmail.com>
2026-09-13 16:23:04 +0600

    Add Avatar component

---
+++ src/pages/6_Avatar.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page padding>
+    <h4 class="text-primary">Avatar Component</h4>
+    <div class="q-gutter-md">
+      <!-- Icon Avatar -->
+      <q-avatar size="80px" color="teal" text-color="white" icon="person" />
+      <!-- Image Avatar -->
+      <q-avatar size="80px">
+        <img src="https://cdn.quasar.dev/img/avatar.png" alt="Avatar" />
+      </q-avatar>
+    </div>
+  </q-page>
+</template>
