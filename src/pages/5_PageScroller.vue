806a9ff12052a49e245f8a7a4a8c24637173195b
TheSamsAlif <samsalif852@gmail.com>
2026-09-13 16:04:40 +0600

    Add Page Scroller

---
+++ src/pages/5_PageScroller.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page padding>
+    <h4>Page Scroller (Back to top)</h4>
+    <p v-for="n in 40" :key="n">Scroll down a bit... {{ n }}</p>
+
+    <!-- Button appears when you scroll down to go back to top -->
+    <q-page-scroller
+      position="bottom-right"
+      :scroll-offset="150"
+      :offset="[18, 18]"
+    >
+      <q-btn fab icon="keyboard_arrow_up" color="primary" />
+    </q-page-scroller>
+  </q-page>
+</template>
