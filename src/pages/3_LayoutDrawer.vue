e35e1263dce95d8abc4f50d7b430c068f87e4bf0
TheSamsAlif <samsalif852@gmail.com>
2026-09-13 16:00:54 +0600

    Add Layout Drawer

---
+++ src/pages/3_LayoutDrawer.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-drawer show-if-above bordered class="bg-grey-2">
+      <q-list>
+        <q-item clickable v-ripple>
+          <q-item-section avatar><q-icon name="home" /></q-item-section>
+          <q-item-section>Home</q-item-section>
+        </q-item>
+        <q-item clickable v-ripple>
+          <q-item-section avatar><q-icon name="settings" /></q-item-section>
+          <q-item-section>Settings</q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <q-page padding>
+        <h4>Layout with Sidebar Drawer</h4>
+      </q-page>
+    </q-page-container>
+  </q-layout>
+</template>
