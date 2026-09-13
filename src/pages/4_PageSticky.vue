b9a00b40b041b46aaee6df45bdfb238692f79179
TheSamsAlif <samsalif852@gmail.com>
2026-09-13 16:03:26 +0600

    Add Page Sticky component

---
+++ src/pages/4_PageSticky.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page padding>
+    <h4>Page Sticky Example</h4>
+    <p v-for="n in 30" :key="n">
+      Scroll down to see the sticky button... {{ n }}
+    </p>
+
+    <!-- This button sticks to the bottom right -->
+    <q-page-sticky position="bottom-right" :offset="[18, 18]">
+      <q-btn fab icon="add" color="accent" />
+    </q-page-sticky>
+  </q-page>
+</template>
