8cf6b875b48d2895e79a50e857c337ca3017871e
TheSamsAlif <samsalif852@gmail.com>
2026-09-13 15:59:54 +0600

    Add Layout Header and Footer

---
+++ src/pages/2_LayoutHeaderFooter.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-toolbar-title>Header Title</q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <q-page padding>
+        <h4>Layout with Header & Footer</h4>
+      </q-page>
+    </q-page-container>
+
+    <q-footer elevated class="bg-grey-8 text-white">
+      <q-toolbar>
+        <q-toolbar-title>Footer Content</q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+  </q-layout>
+</template>
