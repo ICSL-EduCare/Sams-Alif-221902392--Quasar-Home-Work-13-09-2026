5b22d15eba2d5434e81c05014586b5a5c6646569
TheSamsAlif <samsalif852@gmail.com>
2026-09-13 15:55:25 +0600

    Add Flex Grid Layout

---
+++ src/pages/1_FlexGrid.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-page padding>
+    <h4 class="text-primary">Quasar Flex Grid</h4>
+    <div class="row q-col-gutter-md">
+      <div class="col-4">
+        <q-card class="bg-secondary text-white"
+          ><q-card-section>Column 1 (4/12)</q-card-section></q-card
+        >
+      </div>
+      <div class="col-4">
+        <q-card class="bg-accent text-white"
+          ><q-card-section>Column 2 (4/12)</q-card-section></q-card
+        >
+      </div>
+      <div class="col-4">
+        <q-card class="bg-dark text-white"
+          ><q-card-section>Column 3 (4/12)</q-card-section></q-card
+        >
+      </div>
+    </div>
+  </q-page>
+</template>
